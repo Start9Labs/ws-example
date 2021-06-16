d0f0697b5b441dc40a8bb36273c6ce30409b1f99
Aiden McClelland <me@drbonez.dev>
2021-06-16 15:46:19 -0600

    please the needy browser

--- src/main.rs
+++ src/main.rs
@@ -239,7 +239,14 @@ async fn cors<'a, 'b, Params: for<'de> Deserialize<'de> + 'static>(
 ) -> Result<Result<DynMiddlewareStage2<'a, 'b, Params>, Response<Body>>, HttpError> {
     if req.method() == Method::OPTIONS {
         Ok(Err(Response::builder()
-            .header("Access-Control-Allow-Origin", "*")
+            .header(
+                "Access-Control-Allow-Origin",
+                if let Some(origin) = req.headers().get("origin").and_then(|s| s.to_str().ok()) {
+                    origin
+                } else {
+                    "*"
+                },
+            )
             .header("Access-Control-Allow-Methods", "*")
             .header("Access-Control-Allow-Headers", "*")
             .body(Body::empty())?))
