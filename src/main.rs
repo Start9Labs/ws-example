d26308836d9e9adc518ac0ca4e9fa0c8f1f0d713
Aiden McClelland <me@drbonez.dev>
2021-06-16 15:39:57 -0600

    moar cors

--- src/main.rs
+++ src/main.rs
@@ -240,6 +240,8 @@ async fn cors<'a, 'b, Params: for<'de> Deserialize<'de> + 'static>(
     if req.method() == Method::OPTIONS {
         Ok(Err(Response::builder()
             .header("Access-Control-Allow-Origin", "*")
+            .header("Access-Control-Allow-Methods", "*")
+            .header("Access-Control-Allow-Headers", "*")
             .body(Body::empty())?))
     } else {
         Ok(Ok(Box::new(|_req| {
